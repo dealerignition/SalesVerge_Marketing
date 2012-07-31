8645ba50f4f77085b04bda0ee03bd1d973e84312
Justin Wilson <justin@dealerignition.com>
2012-07-31 10:56:28 -0400

    [#33593079] adds some absolute links and HOST constant

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SalesVergeMarketing2::Application.initialize!
+
+if Rails.env.development?
+  HOST = "localhost:3000"
+else
+  HOST = "http://www.salesverge.com"
+end
