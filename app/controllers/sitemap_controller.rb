c656b06f7ef3f6addcd5524135096e64139dc108
Justin Wilson <justintime1889@gmail.com>
2012-08-22 15:13:30 -0400

    fix site map to not include unpublished posts

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -2,7 +2,7 @@ class SitemapController < ActionController::Base
   layout nil
 
   def index
-    @posts = Refinery::Blog::Post.all
+    @posts = Refinery::Blog::Post.where("published_at < ?", Time.now)
     headers['Content-Type'] = 'application/xml'
     respond_to do |format|
       format.xml
